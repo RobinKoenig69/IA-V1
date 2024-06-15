636b554bf389abbc8fd362fe5de2d22bb49662e1
Robin Koenig <116873031+RobinKoenig69@users.noreply.github.com>
2024-06-15 14:20:13 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -17,15 +17,23 @@ model = GPTJForCausalLM.from_pretrained(model_name).to(device)
 print(f"Model is on device: {next(model.parameters()).device}")
 
 # Fonction de génération de texte
-def generate_text(prompt, max_length=2000):
+def generate_text(prompt, max_length=500, temperature=0.7, top_p=0.9, repetition_penalty=1.1):
     inputs = tokenizer(prompt, return_tensors="pt").to(device)
     print(f"Inputs are on device: {inputs.input_ids.device}")
 
-    outputs = model.generate(inputs.input_ids, max_length=max_length, pad_token_id=tokenizer.eos_token_id)
+    outputs = model.generate(
+        inputs.input_ids,
+        do_sample=True,
+        max_length=max_length,
+        pad_token_id=tokenizer.eos_token_id,
+        temperature=temperature,
+        top_p=top_p,
+        repetition_penalty=repetition_penalty
+    )
     print(f"Outputs are on device: {outputs.device}")
 
     return tokenizer.decode(outputs[0], skip_special_tokens=True)
 
 # Exemple d'utilisation
-prompt = "écris moi un paragraphe sur la place de la femme dans la société au 19ème siècle"
+prompt = "Can you explain how the mass of the earth was calculated ?"
 print(generate_text(prompt))
